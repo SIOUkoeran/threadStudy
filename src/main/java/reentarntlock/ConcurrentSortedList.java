92ec5e708c1a6487a5fdb58179d0daa82547355a
SIOUkoeran <alstnalsgud@gmail.com>
2022-07-03 21:07:24 +0900

    Add ConcurrentSortedList size

--- src/main/java/reentarntlock/ConcurrentSortedList.java
+++ src/main/java/reentarntlock/ConcurrentSortedList.java
@@ -48,4 +48,20 @@ public class ConcurrentSortedList {
             next.lock.unlock();
         }
     }
+    public int size(){
+        Node current = tail;
+        int count = 0;
+
+        while (current.prev != head){
+            ReentrantLock lock = current.lock;
+            lock.lock();
+            try{
+                ++count;
+                current = current.prev;
+            }finally {
+                lock.unlock();
+            }
+        }
+        return count;
+    }
 }
